00b0f28b1c53e73ed3078f1618c52ba42edf3304
Siddhant <siddhantsharma669@gmail.com>
2022-02-13 01:26:35 +0530

    revised no Know bugs

--- scorecard.js
+++ scorecard.js
@@ -1,7 +1,6 @@
 const cheerio = require('cheerio');
 const request = require('request');
 
-
 function getVenueAndResult(url){
 
     request(url,function(error , responce , html ){
@@ -19,32 +18,35 @@ function VenueAndResult(html){
     let $ = cheerio.load(html);
     let venarr = $('.match-header-info.match-info-MATCH  .description');
 
-    for( let i =0; i < venarr.length ; i++){
+    // for( let i =0; i < venarr.length ; i++){
 
-        let ven = $(venarr[i]).text().split(",");
+        // let ven = $(venarr[i]).text().split(",");
+        let ven = $(venarr).text().split(",");
         console.log(ven[2]);//Date
         console.log(ven[1]);//Venue
          
-    }
+    // }
 
 
 
     let result = $('.event .status-text > span');
 
-    for( let i =0; i < result.length ; i++){
+    // for( let i =0; i < result.length ; i++){//It will work without this as well as there is only a single element
 
-        let r = $(result[i]).text();
+        // let r = $(result[i]).text();
+        let r = $(result).text();
         console.log(r);
-         
-    }
+        
+    // }
 
     let innings = $('.card.content-block.match-scorecard-table>.Collapsible')//Brings the stats table of the selected team
 
     let htmlString = '';
-
+    // for( let i =0; i<2 ; i++ ){ //This will also work
     for( let i =0; i<innings.length ; i++ ){
 
         htmlString += $(innings[i]).html();
+        // console.log( htmlString);
 
         let teamName = $(innings[i]).find("h5").text();
         teamName = teamName.split('INNINGS')[0].trim();
@@ -56,12 +58,36 @@ function VenueAndResult(html){
         let opponentName = $(innings[opponentindex]).find('h5').text();
         opponentName = opponentName.split('INNINGS')[0].trim();
 
-        console.log(teamName , opponentName);
+        // console.log(teamName , opponentName);
+
+        let cInning = $(innings[i]);
+ 
+        let allRows = cInning.find('.table.batsman tbody tr');
+
+        for( let j=0; j<allRows.length ; j++ ){
+
+            let allCols = $(allRows[j]).find('td');
+            let isWorthy = $(allCols[0]).hasClass('batsman-cell');//Return true 0r false
+ 
+            if( isWorthy == true ){
+
+                let playerName = $(allCols[0]).text().trim();
+                let runs = $(allCols[2]).text().trim();
+                let balls = $(allCols[3]).text().trim();
+                let fours = $(allCols[5]).text().trim();
+                let sixes = $(allCols[6]).text().trim();
+                let STR = $(allCols[7]).text().trim();
+                console.log(`${playerName} | ${runs} | ${balls} | ${fours} | ${sixes} | ${STR}`);
+            }
+
+        }
+
+        console.log(`**************************** *****************************************`)
 
 
     }
 
-    console.log(htmlString)
+    // console.log(htmlString)
 } 
 
 module.exports ={
